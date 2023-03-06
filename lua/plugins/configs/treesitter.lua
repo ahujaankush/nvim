efcafa91c486151a0557a5cc71a82537d44037ee
Ankush Ahuja <aahuja2006@gmail.com>
2023-03-06 21:07:37 +0100

    Init

---
+++ lua/plugins/configs/treesitter.lua
@@ -0,0 +1,24 @@
+local options = {
+  ensure_installed = {
+    "vim",
+    "lua",
+    "html",
+    "css",
+    "javascript",
+    "python",
+    "java",
+    "c",
+    "markdown",
+    "markdown_inline",
+  },
+  indent = {
+    enable = true,
+  },
+
+  highlight = {
+    enable = true,
+    use_languagetree = true,
+  },
+}
+
+return options
